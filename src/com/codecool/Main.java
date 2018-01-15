021385903840f27558d8e34b6bb63ba579eec6b2
Farago Bence <semmiertelme13@gmail.com>
2018-01-15 22:06:13 +0100

    Testing the implemented methods, everythings fine

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,7 +1,22 @@
 package com.codecool;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Outpost myOutpost = Outpost.createOutpost();
+        myOutpost.addTo(new Food("sajt", 20, 35));
+        myOutpost.List("Kecske");
+        if (myOutpost.findItems("sajt") instanceof Food) {
+            System.out.println("Ja fasza");
+        }
+        myOutpost.removeItem("sajt");
+        myOutpost.List("Kecske");
+        int a = (myOutpost.inventory).length;
+        System.out.println(a);
+        School mySchool = School.createSchool();
+        mySchool.listCreatures();
     }
 }
