9200cc32e075b62164298aae1540a66477a0b181
Farago Bence <semmiertelme13@gmail.com>
2018-01-16 13:47:37 +0100

    Now you can fight with monsters

---
+++ src/com/codecool/Fight.java
@@ -0,0 +1,60 @@
+package com.codecool;
+
+import java.util.Scanner;
+
+public class Fight {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    static boolean fighting(Survivor survivor, Creatures enemy, Outpost outpost) {
+        System.out.println("\n"+enemy.getName() + "attacked you");
+        System.out.println("\nhealth: " + enemy.getHealth() + " strength: " + enemy.getStrength());
+        boolean won;
+        int yourStrength = survivor.getStrength();
+        Weapon yourWeapon;
+        while (true) {
+            System.out.println("\nDo you want to use a weapon from your inventory? \n(Y)es or (N)o?");
+            String line = scanner.next();
+            if (line.equals("Y")) {
+                while (true) {
+                    outpost.List("Weapon");
+                    System.out.println("\nEnter the name of the weapon, that you want to use");
+                    line = scanner.next();
+                    if (line.equals("0")){
+                        break;
+                    }
+                    if (outpost.findWeapon(line) != null) {
+                        yourWeapon = outpost.findWeapon(line);
+                        yourWeapon.setDurabillity(-1);
+                        yourStrength += yourWeapon.getStrength();
+                        break;
+                    } else {
+                        System.out.println("\nEnter a weapon name from the list or enter 0 to go back");
+                    }
+                }
+                break;
+            } else if (line.equals("N")) {
+                break;
+            } else {
+                System.out.println("\nWrong input enter Y or N");
+            }
+        }
+
+        while (true) {
+            enemy.setHealth(0 - yourStrength);
+            if (enemy.getHealth() < 1) {
+                System.out.println("\nCongratulations, you won the fight");
+                won = true;
+                break;
+            }
+            survivor.setHealth(0 - enemy.getStrength());
+            if (survivor.getHealth() < 1) {
+                System.out.println("\nYou lost the fight, your Survivor is dead");
+                won = false;
+                break;
+            }
+        }
+        survivor.setActionPoints(-1);
+        return won;
+    }
+}
