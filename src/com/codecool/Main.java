03339481a488fcfc5b4658f3e7214f7146847f8a
Farago Bence <semmiertelme13@gmail.com>
2018-01-18 19:24:09 +0100

    Started implementing the menu

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -5,22 +5,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws FileNotFoundException {
-        Outpost myOutpost = Outpost.createOutpost();
-        Survivor mySurvivor = new Survivor("Jani", 2, 100, 100, 0, 5, "Outpost");
-        myOutpost.addTo(new Food("sajt", 20, 35));
-        myOutpost.addTo(new Food("sajt", 20, 35));
-        myOutpost.addTo(new Weapon("reszelo", 5, 2));
-        myOutpost.List("Food");
-
-        myOutpost.List("Inventory");
-
-        myOutpost.removeItem("sajt");
-        myOutpost.List("Food");
-        System.out.println((myOutpost.inventory).length);
-        School mySchool = School.createSchool();
-        mySchool.listCreatures();
-        mySchool.search(mySurvivor, myOutpost);
-        myOutpost.List("Inventory");
+        System.out.println("1.New Game\n2. Load Game");
+        String line = scanner.next();
+        if (line.equals("1")){
+            Outpost myOutpost = Outpost.createOutpost();
+            School mySchool = School.createSchool();
+            GasStation myGasStation = GasStation.createGasStation();
+            Hospital myHospital = Hospital.createHospital();
+            MilitaryBase myMilitaryBase = MilitaryBase.createMilitaryBase();
+            System.out.print("You must create a Survivor first. Enter his name!\n");
+            line = scanner.next();
+            Survivor mySurvivor = new Survivor(line, 2, 100, 100, 0, 5, "Outpost");
+            mySurvivor.printAttributes();
+            myOutpost.printMenu();
+            mySchool.search(mySurvivor, myOutpost);
+            myMilitaryBase.search(mySurvivor, myOutpost);
+            myHospital.search(mySurvivor, myOutpost);
+            myGasStation.search(mySurvivor, myOutpost);
+        }
+        // Need load game src-s,game saving and else if incorrect input added
     }
 }
