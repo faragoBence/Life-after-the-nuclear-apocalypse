b06a6584624e90f1c4bfc4bb5f85aa63d2f1e360
Farago Bence <semmiertelme13@gmail.com>
2018-05-05 16:36:11 +0200

    Survivor factory enum names replaced with the new names.

--- src/main/java/com/codecool/web/model/survivors/SurvivorFactoryImpl.java
+++ src/main/java/com/codecool/web/model/survivors/SurvivorFactoryImpl.java
@@ -6,12 +6,12 @@ public class SurvivorFactoryImpl implements SurvivorFactory{
     public Survivor createSurvivor(String name, String type) {
         Class classEnum = Class.valueOf(type);
         switch (classEnum){
-            case MAGE:
-                return new Survivor(name,3,3,80,80,75,100,100,3,10,"OUTPOST","Mage");
-            case HUNTER:
-                return new Survivor(name,2,2,100,100,80,90,90,5,8,"OUTPOST","Hunter");
-            case WARRIOR:
-                return new Survivor(name,2,2,80,80,100,80,80,7,5,"OUTPOST","Warrior");
+            case GUNSLINGER:
+                return new Survivor(name,3,3,80,80,75,100,100,3,10,"OUTPOST","Gunslinger");
+            case SOLDIER:
+                return new Survivor(name,2,2,100,100,80,90,90,5,8,"OUTPOST","Soldier");
+            case BRUISER:
+                return new Survivor(name,2,2,80,80,100,80,80,7,5,"OUTPOST","Bruiser");
             default:
                 return null;
         }
