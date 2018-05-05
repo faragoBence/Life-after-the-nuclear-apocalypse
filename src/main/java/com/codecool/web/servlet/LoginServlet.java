bb7b0937bd9ca32ab6582b779d41e4e0b591f5aa
Farago Bence <semmiertelme13@gmail.com>
2018-05-05 14:26:11 +0200

    After login the program gives back the Survivor object too

--- src/main/java/com/codecool/web/servlet/LoginServlet.java
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -1,10 +1,17 @@
 package com.codecool.web.servlet;
 
 
+import com.codecool.web.dao.SurvivorDao;
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.implementations.SurvivorDatabaseDao;
 import com.codecool.web.dao.implementations.UserDatabaseDao;
+import com.codecool.web.dto.UserSurvivorDto;
 import com.codecool.web.exception.UserNotFoundException;
 import com.codecool.web.exception.WrongPasswordException;
 import com.codecool.web.model.User;
+import com.codecool.web.model.survivors.Survivor;
+import com.codecool.web.service.SurvivorService;
+import com.codecool.web.service.implementations.SurvivorServiceImpl;
 import com.codecool.web.service.implementations.UserServiceImpl;
 
 import javax.servlet.ServletException;
@@ -27,11 +34,15 @@ public class LoginServlet extends AbstractServlet {
         String password = req.getParameter("password");
 
         try (Connection connection = getConnection(req.getServletContext())) {
-            UserDatabaseDao userDao = new UserDatabaseDao(connection);
+            UserDao userDao = new UserDatabaseDao(connection);
+            SurvivorDao survivorDao = new SurvivorDatabaseDao(connection);
             UserServiceImpl userServiceDao = new UserServiceImpl(userDao);
+            SurvivorService survivorService = new SurvivorServiceImpl(survivorDao);
             User currentUser = userServiceDao.login(email, password);
+            Survivor survivor = survivorService.findSurvivor(currentUser);
             req.getSession().setAttribute("user", currentUser);
-            sendMessage(resp, HttpServletResponse.SC_OK, currentUser);
+            req.getSession().setAttribute("survivor", survivor);
+            sendMessage(resp, HttpServletResponse.SC_OK, new UserSurvivorDto(currentUser,survivor));
         } catch (SQLException e) {
             handleSqlError(resp,e);
         } catch (UserNotFoundException e) {
