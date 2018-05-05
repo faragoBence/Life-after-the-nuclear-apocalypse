e477f01b8a621bd9090925bdb6265c39d606f29b
Farago Bence <semmiertelme13@gmail.com>
2018-05-05 16:31:48 +0200

    Resource model class refactored

--- src/main/java/com/codecool/web/model/items/Resource.java
+++ src/main/java/com/codecool/web/model/items/Resource.java
@@ -1,20 +1,9 @@
 package com.codecool.web.model.items;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Resource extends Item {
     public Resource(String name){
         super (name);
     }
 
-    public static List<Resource> createResourceList(){
-        List<Resource> resourceList = new ArrayList<>();
-        resourceList.add(new Resource("Scrap metal"));
-        resourceList.add(new Resource("Wood"));
-        resourceList.add(new Resource("Chemical"));
-        resourceList.add(new Resource("Food"));
-        resourceList.add(new Resource("Wool"));
-        return resourceList;
-    }
+
 }
