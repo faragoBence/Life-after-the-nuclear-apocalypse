7a0c3d4301c0a303b3cb1697ea3dd307402ad0ea
Farago Bence <semmiertelme13@gmail.com>
2018-05-05 14:25:12 +0200

    After registration a Survivor is created too.

--- src/main/java/com/codecool/web/servlet/RegisterServlet.java
+++ src/main/java/com/codecool/web/servlet/RegisterServlet.java
@@ -1,7 +1,16 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.SurvivorDao;
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.implementations.SurvivorDatabaseDao;
 import com.codecool.web.dao.implementations.UserDatabaseDao;
 import com.codecool.web.exception.UserAlreadyRegisteredException;
+import com.codecool.web.exception.UserNotFoundException;
+import com.codecool.web.exception.WrongPasswordException;
+import com.codecool.web.model.User;
+import com.codecool.web.service.SurvivorService;
+import com.codecool.web.service.UserService;
+import com.codecool.web.service.implementations.SurvivorServiceImpl;
 import com.codecool.web.service.implementations.UserServiceImpl;
 
 import javax.servlet.ServletException;
@@ -22,16 +31,26 @@ public class RegisterServlet extends AbstractServlet {
         String name = req.getParameter("name");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+        String survivorName = req.getParameter("survivorName");
+        String type = req.getParameter("type");
 
         try (Connection connection = getConnection(req.getServletContext())) {
-            UserDatabaseDao userDao = new UserDatabaseDao(connection);
-            UserServiceImpl userServiceDao = new UserServiceImpl(userDao);
-            userServiceDao.register(name, email, password);
+            UserDao userDao = new UserDatabaseDao(connection);
+            SurvivorDao survivorDao = new SurvivorDatabaseDao(connection);
+            UserService userService = new UserServiceImpl(userDao);
+            SurvivorService survivorService = new SurvivorServiceImpl(survivorDao);
+            userService.register(name, email, password);
+            User user = userService.login(email, password);
+            survivorService.createSurvivor(survivorName, type.toUpperCase(), user.getId());
             sendMessage(resp, HttpServletResponse.SC_OK, "Registration is successful you can log in now!");
         } catch (SQLException e) {
             handleSqlError(resp, e);
         } catch (UserAlreadyRegisteredException e) {
             sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, "You are already registered!");
+        } catch (UserNotFoundException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, "User not found! Please register first!");
+        } catch (WrongPasswordException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, "Wrong password entered!");
         }
     }
 }
