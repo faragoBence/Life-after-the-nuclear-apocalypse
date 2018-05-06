502259b6c960760b25cdc43828a002607c7456bd
Farago Bence <semmiertelme13@gmail.com>
2018-05-06 16:29:45 +0200

    Survivor Dto contains Outpost too

--- src/main/java/com/codecool/web/dto/SurvivorDto.java
+++ src/main/java/com/codecool/web/dto/SurvivorDto.java
@@ -1,15 +1,18 @@
 package com.codecool.web.dto;
 
 import com.codecool.web.model.User;
+import com.codecool.web.model.locations.Outpost;
 import com.codecool.web.model.survivors.Survivor;
 
-public class UserSurvivorDto {
+public class SurvivorDto {
     private final User user;
     private final Survivor survivor;
+    private final Outpost outpost;
 
-    public UserSurvivorDto(User user, Survivor survivor) {
+    public SurvivorDto(User user, Survivor survivor, Outpost outpost) {
         this.user = user;
         this.survivor = survivor;
+        this.outpost = outpost;
     }
 
     public User getUser() {
@@ -18,5 +21,9 @@ public class UserSurvivorDto {
 
     public Survivor getSurvivor() {
         return survivor;
+    }
+
+    public Outpost getOutpost() {
+        return outpost;
     }
 }
